af750bf2d679d646650b30481961792d056aec65
Amir thapa magar <amirthapa2019@gmail.com>
2022-12-21 17:21:09 +0545

    feat:login fixed

--- src/sections/login/loginPage.js
+++ src/sections/login/loginPage.js
@@ -3,61 +3,61 @@ import React from "react";
 import { Container } from "@mui/system";
 import { Icon } from "@iconify/react";
 import { useTheme } from "@emotion/react";
-import { PrimaryButton, SecondaryButton } from "@components/Button";
-export default function Login() {
-    const theme = useTheme();
-    const router = useRouter();
-    const { addPublicAddress, addWallet } = useAppAuthContext();
-  
-    const handleCreateWallet = async () => {
-      const wallet = await library.createWallet();
-      addPublicAddress(wallet.address);
-      addWallet(wallet);
-    };
-  
-    return (
-      <Container>
-        <Typography variant="h3" sx={{ mt: 5, pb: 2 }}>
-         Log in using your google account
-        </Typography>
-        <Box
-          sx={{
-            border: 0.1,
-            p: 2,
-            borderRadius: 1,
-          }}
-        >
-          <PrimaryButton onClick={handleCreateWallet}>
-            <Icon
-              icon="fluent:wallet-credit-card-24-filled"
-              height={30}
-              width={40}
-            />
-          Google sign in
-          </PrimaryButton>
-        </Box>
-        <hr />
-        <Typography variant="h5" sx={{ mt: 2, pb: 2 }}>
-          or
-        </Typography>
-        <Box
-          sx={{
-            border: 0.1,
-            p: 2,
-            borderRadius: 1,
-          }}
-        >
-    
-          <SecondaryButton onClick={handleRestoreWallet}>
-            <Icon
-              icon="akar-icons:google-fill"
-              height={30}
-              width={40}
-              color={theme.palette.primary.lighter}
-            />
-            Google drive
-          </SecondaryButton>
-        </Box>
-      </Container>
-    );
-  }
+import { useRouter } from "next/router";
+// import { PrimaryButton, SecondaryButton } from "@components/Button";
+export default function LoginPage() {
+  const theme = useTheme();
+  const router = useRouter();
+  // const { addPublicAddress, addWallet } = useAppAuthContext();
+
+  const handleCreateWallet = async () => {
+    const wallet = await library.createWallet();
+    addPublicAddress(wallet.address);
+    addWallet(wallet);
+  };
+
+  return (
+    <Container>
+      <Typography variant="h3" sx={{ mt: 5, pb: 2 }}>
+        Log in using your google account
+      </Typography>
+      <Box
+        sx={{
+          border: 0.1,
+          p: 2,
+          borderRadius: 1,
+        }}
+      >
+        {/* <PrimaryButton onClick={handleCreateWallet}> */}
+        <Icon
+          icon="fluent:wallet-credit-card-24-filled"
+          height={30}
+          width={40}
+        />
+        Google sign in
+        {/* </PrimaryButton> */}
+      </Box>
+      <hr />
+      <Typography variant="h5" sx={{ mt: 2, pb: 2 }}>
+        or
+      </Typography>
+      <Box
+        sx={{
+          border: 0.1,
+          p: 2,
+          borderRadius: 1,
+        }}
+      >
+        {/* <SecondaryButton onClick={handleRestoreWallet}> */}
+        <Icon
+          icon="akar-icons:google-fill"
+          height={30}
+          width={40}
+          color={theme.palette.primary.lighter}
+        />
+        Google drive
+        {/* </SecondaryButton> */}
+      </Box>
+    </Container>
+  );
+}
