0c173d9d81017d9c72903633164f8c73f1fc36aa
Amir thapa magar <amirthapa2019@gmail.com>
2022-12-26 15:28:31 +0545

    feat:commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Page from "@components/Page";
 import DashboardLayout from "@layouts/dashboard/DashboardLayout";
 import DashboardLayoutwithFooter from "@layouts/dashboard/DashboardlayoutwithFooter";
-
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 const PAGE_TITLE = "Home";
 
 Home.getLayout = (page) => (
@@ -13,5 +14,24 @@ Home.getLayout = (page) => (
 );
 
 export default function Home() {
+  async function main() {
+    const user = await prisma.user.create({
+      data: {
+        firstname: "test",
+        lastname: "test",
+        email: "test",
+        phone: "test",
+        userethaddres: "test",
+      },
+    });
+    console.log(user);
+  }
+
+  main()
+    .catch((e) => {})
+    .finally(async () => {
+      await prisma.$disconnect();
+    });
+
   return <Page title={PAGE_TITLE}>Event App</Page>;
 }
