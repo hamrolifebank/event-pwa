59ec7a52936848f057c6fe9ce796be52fe3673cc
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-01-05 14:32:38 +0545

    feat: event server created

---
+++ src/pages/api/event/index.js
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({ log: ["query"] });
+export default async function handler(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      try {
+        const events = await prisma.event.findMany();
+
+        res.status(200).json({ success: true, data: events });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    case "POST":
+      try {
+        const event = await prisma.event.create({
+          data: req.body,
+        });
+
+        res.status(201).json({ success: true, data: event });
+      } catch (error) {
+        res.status(400).json({ success: false });
+      }
+      break;
+    default:
+      res.status(400).json({ success: false });
+      break;
+  }
+}
