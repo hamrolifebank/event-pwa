2ad7e91bac84cf260e79b122de6a8af588a25a70
Amir thapa magar <amirthapa2019@gmail.com>
2023-01-07 21:24:38 +0545

    feat:made authguard

--- src/guards/AuthGuard.js
+++ src/guards/AuthGuard.js
@@ -13,48 +13,20 @@ AuthGuard.propTypes = {
 // Wrap this for all pages that require authentication
 
 export default function AuthGuard({ children }) {
-  // temporary --- remove this
-  // if (certainCondition) {
-  //   return <>{children}</>;
-  // }
-
-  // redirect to login page if not authenticated
-
-  // const { isAuthenticated, isInitialized } = useAppAuthContext();
-
-  // const { pathname, push } = useRouter();
-
-  // const [requestedLocation, setRequestedLocation] = useState(null);
-
-  // useEffect(() => {
-  //   if (requestedLocation && pathname !== requestedLocation) {
-  //     push(requestedLocation);
-  //   }
-  //   if (isAuthenticated) {
-  //     setRequestedLocation(null);
-  //   }
-  // }, [isAuthenticated, pathname, push, requestedLocation]);
-
-  // if (!isInitialized) {
-  //   return <LoadingScreen />;
-  // }
-
-  // if (!isAuthenticated) {
-  //   if (pathname !== requestedLocation) {
-  //     setRequestedLocation(pathname);
-  //   }
-  //   return <Login />;
-  // }
   const user = useSelector((state) => state.user);
   const { pathname, push } = useRouter();
 
   useEffect(() => {
-    if (user) {
-      push("/");
-    } else {
+    if (user === null) {
       push(PATH_AUTH.login);
     }
   }, [user]);
 
-  return <>{children}</>;
+  if (!user) {
+    if (pathname === PATH_AUTH.login) {
+      return <>{children}</>;
+    } else {
+      return <></>;
+    }
+  } else return <>{children}</>;
 }
