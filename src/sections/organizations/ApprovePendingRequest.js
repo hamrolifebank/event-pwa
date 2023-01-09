0c2d76c2a660904a881f9c893521862e96096e5d
pitamberrana <pitamber11rana@gmail.com>
2023-01-09 14:54:39 +0545

    feat: add Autocomplete component

--- src/sections/organizations/ApprovePendingRequest.js
+++ src/sections/organizations/ApprovePendingRequest.js
@@ -1,13 +1,33 @@
-import { Container, Grid, IconButton, Stack, Typography } from "@mui/material";
+import {
+  Autocomplete,
+  Container,
+  Grid,
+  IconButton,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import BorderlessButton from "@components/button/BorderlessButton";
 import { useRouter } from "next/router";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
-import PendingReqCard from "@sections/event-card/pending-req-card";
 import ApprovePendingReqCard from "@sections/event-card/ApprovePendingReqCard";
 
 const ApprovePendingRequest = () => {
+  const [input, setInput] = React.useState("");
   const router = useRouter();
+
+  const handleInput = (e) => {
+    setInput(e.target.value.toLowerCase());
+  };
+  let org = [];
+  const filteredList = org.filter((list) => {
+    if (input === "") {
+      return list;
+    } else {
+      return list.label.toLowerCase().includes(input);
+    }
+  });
   return (
     <Container>
       <IconButton color="primary" onClick={() => router.back()}>
@@ -21,7 +41,22 @@ const ApprovePendingRequest = () => {
         </Grid>
       </Grid>
 
+      <Autocomplete
+        disablePortal
+        id="filter-box"
+        options={org.map((list) => list.label)}
+        fullWidth
+        sx={{ mt: 2 }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Filter by person or organization name"
+          />
+        )}
+      />
+
       <Stack spacing={1} mt={2}>
+        {/* {filteredList.map((list)=>.....)} */}
         <ApprovePendingReqCard />
         <ApprovePendingReqCard />
         <ApprovePendingReqCard />
