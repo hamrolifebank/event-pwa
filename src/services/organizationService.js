fc18877b2ca8df01309c704e7e7c51b704249834
Sudesh Mate <sudesh7443@gmail.com>
2023-01-11 11:48:43 +0545

    feat: change the url in axios

--- src/services/organizationService.js
+++ src/services/organizationService.js
@@ -1,32 +1,35 @@
 import api from "./client";
+import axios from "axios";
 
 const getAll = async () => {
-  const organizations = await api.get("/organization");
+  const organizations = await axois.get("/api/organization");
 
   return organizations.data.data;
 };
 
 const getMyPendingRequests = async () => {
-  const myPendingRequests = await api.get("/organization/my-pendingrequests");
+  const myPendingRequests = await axios.get(
+    "/api/organization/my-pendingrequests"
+  );
 
   return myPendingRequests.data.data;
 };
 
 const getMyNotJoinedOrganizations = async () => {
-  const myNotJoinedOrganizations = await api.get(
-    "/organization/my-notjoinedorg"
+  const myNotJoinedOrganizations = await axios.get(
+    "/api/organization/my-notjoinedorg"
   );
   return myNotJoinedOrganizations.data.data;
 };
 
 const create = async (organization) => {
-  const newOrganization = await api.post("/organization", organization);
+  const newOrganization = await axios.post("/api/organization", organization);
 
   return newOrganization.data.data;
 };
 
 const join = async (organizationId) => {
-  const joinedOrganization = await api.post(`/organization/join`, {
+  const joinedOrganization = await axios.post(`/api/organization/join`, {
     organizationId,
   });
 
