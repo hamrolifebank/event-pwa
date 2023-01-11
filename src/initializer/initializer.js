01e88505ffd83443a4098fb7c8162ab3a523412b
SurazGaire <luxheartsuraz@gmail.com>
2023-01-11 09:30:22 +0545

    feat : initialize data in redux store  at start and use them at components

--- src/initializer/initializer.js
+++ src/initializer/initializer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
-const Initializer = ({ children }) => {
+import { getAllEvents } from "@redux/reducers/eventReducer";
+
+export default function Initializer({ children }) {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+  useEffect(() => {
+    if (user) {
+      dispatch(getAllEvents());
+      dispatch(initializeYourNotJoinedOrganizations());
+    }
+  }, [dispatch]);
   return <>{children}</>;
-};
-
-export default Initializer;
+}
--- src/sections/events/UpcomingEvents.js
+++ src/sections/events/UpcomingEvents.js
@@ -5,6 +5,7 @@ import { EventCard } from "@sections/event-card";
 import React from "react";
 
 const UpcomingEvents = () => {
+  const events = useSelector((state) => state.events);
   return (
     <Container>
       <Typography
