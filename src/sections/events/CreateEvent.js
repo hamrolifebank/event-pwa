036f1612a470ca812075933dd8b0b3d8ac680f99
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-01-12 22:20:35 +0545

    feat: type converted to number

--- src/sections/events/CreateEvent.js
+++ src/sections/events/CreateEvent.js
@@ -178,7 +178,7 @@ const CreateEvent = () => {
           />
           <TextField
             label="Target"
-            type="text"
+            type="number"
             name="noOfTarget"
             value={field.noOfTarget}
             onChange={handleInput}
@@ -208,7 +208,7 @@ const CreateEvent = () => {
           />
           <TextField
             label="Latitude"
-            type="text"
+            type="number"
             name="latitude"
             value={field.latitude}
             onChange={handleInput}
@@ -223,7 +223,7 @@ const CreateEvent = () => {
           />
           <TextField
             label="Longitude"
-            type="text"
+            type="number"
             name="longitude"
             value={field.longitude}
             onChange={handleInput}
