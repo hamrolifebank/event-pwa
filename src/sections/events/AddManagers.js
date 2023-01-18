9835f46ae53e7b5502ce4f3a4dd3ff3ea995b00b
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-01-18 14:35:54 +0545

    feat: add manager section created

---
+++ src/sections/events/AddManagers.js
@@ -0,0 +1,46 @@
+import { BorderlessButton, PrimaryButton } from "@components/button";
+import { Typography } from "@mui/material";
+import { Container, Box } from "@mui/system";
+import ManagerCard from "@sections/event-card/ManagerCard";
+import React from "react";
+
+const AddManagers = () => {
+  return (
+    <Container>
+      <Typography
+        display="flex"
+        justifyContent="center"
+        variant="h4"
+        sx={{ mt: 4 }}
+        color="black"
+      >
+        HBL Doner center
+      </Typography>
+      <PrimaryButton sx={{ mt: 2, mb: 2 }}>Add manager</PrimaryButton>
+      <Typography
+        display="flex"
+        justifyContent="center"
+        sx={{
+          mb: 2,
+          fontSize: "subtitle1.fontSize",
+          fontWeight: "subtitle1.fontWeight",
+          lineHeight: "subtitle1.lineHeight",
+        }}
+        color="grey.600"
+      >
+        CURRENT MANAGER
+      </Typography>
+      <ManagerCard />
+      <ManagerCard />
+      <ManagerCard />
+
+      <Box>
+        <BorderlessButton sx={{ mt: 2, mb: 2, color: "secondary.main" }}>
+          Load More Managers
+        </BorderlessButton>
+      </Box>
+    </Container>
+  );
+};
+
+export default AddManagers;
