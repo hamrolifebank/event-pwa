1bc9594f83e1c4f46b93695a5d199c1222119715
rajutamanglama63 <rajutamanglama63@gmail.com>
2023-01-31 15:51:39 +0545

    feat: form reconstruced

--- src/sections/event-card/PledgersCard.js
+++ src/sections/event-card/PledgersCard.js
@@ -1,5 +1,17 @@
-import { Paper, Typography } from "@mui/material";
-import { Box } from "@mui/system";
+import {
+  Button,
+  Paper,
+  Modal,
+  Typography,
+  InputAdornment,
+  TextField,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import { PrimaryButton, SecondaryButton } from "@components/button";
 import { Icon } from "@iconify/react";
 import React, { useEffect, useState } from "react";
 import EthCrypto from "eth-crypto";
@@ -7,6 +19,20 @@ import EthCrypto from "eth-crypto";
 const PledgersCard = ({ pledgers, privateKey }) => {
   const [data, setData] = useState(null);
   const pledgerInfo = JSON.parse(pledgers);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const typeOfConsent = [
+    { id: 1, type: "IPFS" },
+    { id: 2, type: "Image" },
+  ];
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setField({ ...field, [name]: value });
+  };
 
   const decryptedData = async () => {
     const random = await EthCrypto.decryptWithPrivateKey(privateKey, {
@@ -66,7 +92,105 @@ const PledgersCard = ({ pledgers, privateKey }) => {
             {data.phone}
           </Typography>
         </Box>
+        <Box>
+          <Button
+            aria-describedby="conform"
+            variant="contained"
+            onClick={() => handleOpen()}
+          >
+            Accept
+          </Button>
+        </Box>
       </Paper>
+
+      <Modal
+        open={open}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "80%",
+            bgcolor: "background.paper",
+            boxShadow: 30,
+            borderRadius: 2,
+            p: 4,
+          }}
+        >
+          <Typography
+            display="flex"
+            justifyContent="center"
+            variant="h3"
+            sx={{ mb: 2 }}
+          >
+            Consent Form
+          </Typography>
+          <Stack spacing={2} component="form" sx={{ mb: 2 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">
+                Consent Type
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                type="text"
+                name="consentType"
+                label="Consent Type"
+                value=""
+                required
+              >
+                {typeOfConsent?.map((consent) => (
+                  <MenuItem key={consent.id} value={consent.type}>
+                    {consent.type}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <TextField
+              label="Consent Value"
+              type="text"
+              name="concentValue"
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icon icon="icon-park-outline:agreement" />
+                  </InputAdornment>
+                ),
+              }}
+            />
+
+            <TextField
+              label="Blood Bag Number"
+              type="number"
+              name="bloodBagNumber"
+              required
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <Icon icon="mdi:blood-bag" />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Stack>
+
+          <Stack id="modal-description" sx={{ mt: 2 }}>
+            <SecondaryButton
+              onClick={() => {
+                handleClose();
+              }}
+            >
+              Confirm
+            </SecondaryButton>
+          </Stack>
+        </Box>
+      </Modal>
     </>
   );
 };
